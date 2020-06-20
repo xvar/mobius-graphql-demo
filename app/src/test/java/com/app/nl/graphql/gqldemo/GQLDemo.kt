d8722e23a7164f3af7aa3d054711059d29dfdfbd
Alexey Khaiminov <a.khaiminov@corp.mail.ru>
2020-06-21 00:50:11 +0300

    apollo example

--- app/src/test/java/com/app/nl/graphql/gqldemo/GQLDemo.kt
+++ app/src/test/java/com/app/nl/graphql/gqldemo/GQLDemo.kt
@@ -1,5 +1,7 @@
 package com.app.nl.graphql.gqldemo
 
+import com.apollographql.apollo.ApolloClient
+import com.apollographql.apollo.rx3.Rx3Apollo
 import okhttp3.MediaType.Companion.toMediaTypeOrNull
 import okhttp3.OkHttpClient
 import okhttp3.Request
@@ -9,14 +11,20 @@ import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 import java.io.IOException
 
-
+/**
+ * Important note: functions below are not the "tests" by any means.
+ * First of all: they're intentionally failing. :)
+ * Second: you wouldn't "test" api like that.
+ *
+ * It's just the fastest way to model some actual query.
+ * Also, it can be helpful when you're debugging some api issues.
+ */
 class GQLDemo {
 
     //just for simplicity (not production code)
     @Suppress("SameParameterValue")
     @Throws(IOException::class)
     private fun post(url: String, content: String, contentType: String? = null): String? {
-        val client = OkHttpClient()
         val body: RequestBody = content.toRequestBody(contentType?.toMediaTypeOrNull())
         val request: Request = Request.Builder()
             .url(url)
@@ -27,39 +35,19 @@ class GQLDemo {
 
     companion object {
         private const val url = "https://swapi-graphql.netlify.app/.netlify/functions/index"
-        private const val FIRST_TWO_PEOPLE : String =
-            "{" +
-                "\"data\":{" +
-                    "\"allPeople\":{" +
-                        "\"people\":[" +
-                            "{" +
-                                "\"name\":\"Luke Skywalker\"," +
-                                "\"birthYear\":\"19BBY\"," +
-                                "\"eyeColor\":\"blue\"," +
-                                "\"homeworld\":" +
-                                    "{" +
-                                        "\"name\":\"Tatooine\"" +
-                                    "}" +
-                            "}," +
-                            "{" +
-                                "\"name\":\"C-3PO\"," +
-                                "\"birthYear\":\"112BBY\"," +
-                                "\"eyeColor\":\"yellow\"," +
-                                "\"homeworld\":" +
-                                    "{" +
-                                        "\"name\":\"Tatooine\"" +
-                                    "}" +
-                            "}" +
-                        "]" +
-                    "}" +
-                "}" +
-            "}"
-
+        private val client = OkHttpClient()
+        private val apolloClient = ApolloClient.builder()
+            .okHttpClient(client)
+            .serverUrl(url)
+            .build()
     }
 
 
+    /**
+     * Request via good old pal - okhttp :)
+     */
     @Test
-    fun swapi_request() {
+    fun swapi_first_two_people_request() {
         val query = "query {\n" +
                 "  allPeople(first: 2) {\n" +
                 "    people {\n" +
@@ -74,9 +62,30 @@ class GQLDemo {
                 "}"
         val request = "$url?query=$query"
         Assertions.assertEquals(
-            FIRST_TWO_PEOPLE,
+            "",
             post(request, "")
         )
+    }
+
+    /**
+     * Same request using apollo
+     */
+    @Test
+    fun swapi_first_two_people_apollo() {
+        val firstTwoPeopleQuery = FirstTwoPeopleQuery()
+        val result = Rx3Apollo.from(apolloClient.query(firstTwoPeopleQuery)).blockingFirst()
+        Assertions.assertEquals("", result.data.toString())
+    }
+
+    /**
+     * Example of request variables and fragments
+     */
+    @Test
+    fun swapi_connecting_people_with_params() {
+        val numPeopleToCollect = 1
+        val peopleConnectionQuery = ConnectedPeopleQuery(numPeopleToCollect)
+        val result = Rx3Apollo.from(apolloClient.query(peopleConnectionQuery)).blockingFirst()
+        Assertions.assertEquals("", result.data.toString())
     }
 
 }
